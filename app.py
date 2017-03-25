19a055f5f8d56660012c18e9f221f00316eb96d2
comyn <me@xueming.li>
2017-03-25 10:21:52 +0800

    [04] 使用wsgify装饰器

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import webob
+from webob.dec import wsgify
 
 
-def application(environ: dict, start_response):
-    request = webob.Request(environ)
+@wsgify
+def application(request: webob.Request) -> webob.Response:
     name = request.params.get("name", 'anonymous')
 
     response = webob.Response()
     response.text = 'hello {}'.format(name)
     response.status_code = 200
     response.content_type = 'text/plain'
-    return response(environ, start_response)
+    return response
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
