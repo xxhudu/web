0119ca6a2150e1aacade2935bce75d7b478ec924
comyn <me@xueming.li>
2017-03-25 11:35:25 +0800

    [09]使用装饰器注册路由

--- app.py
+++ app.py
@@ -3,6 +3,25 @@ from webob import exc
 from webob.dec import wsgify
 
 
+class Application:
+    ROUTER = {}
+
+    @classmethod
+    def register(cls, path):
+        def wrap(handler):
+            cls.ROUTER[path] = handler
+            return handler
+        return wrap
+
+    @wsgify
+    def __call__(self, request: Request) -> Response:
+        try:
+            return self.ROUTER[request.path](request)
+        except KeyError:
+            raise exc.HTTPNotFound('not found')
+
+
+@Application.register('/hello')
 def hello(request: Request) -> Response:
     name = request.params.get("name", 'anonymous')
     response = Response()
@@ -12,35 +31,12 @@ def hello(request: Request) -> Response:
     return response
 
 
+@Application.register('/')
 def index(request: Request) -> Response:
     return Response(body='hello world', content_type='text/plain')
 
-
-router = {
-    '/hello': hello,
-    '/': index
-}
-
-
-class Application:
-    ROUTER = {}
-
-    @classmethod
-    def register(cls, path, handler):
-        cls.ROUTER[path] = handler
-
-    @wsgify
-    def __call__(self, request: Request) -> Response:
-        try:
-            return self.ROUTER[request.path](request)
-        except KeyError:
-            raise exc.HTTPNotFound('not found')
-
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-
-    Application.register('/hello', hello)
-    Application.register('/', index)
 
     server = make_server('0.0.0.0', 8000, Application())
     try:
