ea2fe8d634b9027013add172c41567567ad6066b
comyn <me@xueming.li>
2017-03-25 10:54:28 +0800

    [06]最基本的路由

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from webob.dec import wsgify
 
 @wsgify
 def application(request: webob.Request) -> webob.Response:
-    name = request.params.get("name", 'anonymous')
-
-    response = webob.Response()
-    response.text = 'hello {}'.format(name)
-    response.status_code = 200
-    response.content_type = 'text/plain'
-    return response
+    if request.path == '/hello':
+        name = request.params.get("name", 'anonymous')
+        response = webob.Response()
+        response.text = 'hello {}'.format(name)
+        response.status_code = 200
+        response.content_type = 'text/plain'
+        return response
+    return webob.Response(body='hello world', content_type='text/plain')
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
@@ -20,3 +21,7 @@ if __name__ == '__main__':
         server.serve_forever()
     except KeyboardInterrupt:
         server.shutdown()
+
+
+# /hello?name=comyn => hello comyn
+# / => hello world
