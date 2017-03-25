cfbda85a3fb853dbc3c68bd3a642fdcedcf58c2e
comyn <me@xueming.li>
2017-03-25 15:15:42 +0800

    [11]支持正则捕获

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ from webob import exc
 from webob.dec import wsgify
 
 
+class _Vars:
+    def __init__(self, data=None):
+        if data is not None:
+            self._data = data
+        else:
+            self._data = {}
+
+    def __getattr__(self, item):
+        try:
+            return self._data[item]
+        except KeyError:
+            raise AttributeError('no attribute {}'.format(item))
+
+    def __setattr__(self, key, value):
+        if key != '_data':
+            raise NotImplemented
+        self.__dict__['_data'] = value
+
+
 class Application:
     ROUTER = []
 
@@ -17,14 +36,18 @@ class Application:
     @wsgify
     def __call__(self, request: Request) -> Response:
         for pattern, handler in self.ROUTER:
-            if pattern.match(request.path):
+            m = pattern.match(request.path)
+            if m:
+                request.args = m.groups()
+                request.kwargs = _Vars(m.groupdict())
                 return handler(request)
         raise exc.HTTPNotFound('not found')
 
 
-@Application.register('^/hello$')
+@Application.register('^/hello/(?P<name>\w+)$')
 def hello(request: Request) -> Response:
-    name = request.params.get("name", 'anonymous')
+    # name = request.params.get("name", 'anonymous')
+    name = request.kwargs.name
     response = Response()
     response.text = 'hello {}'.format(name)
     response.status_code = 200
@@ -48,3 +71,6 @@ if __name__ == '__main__':
 
 # /hello?name=comyn => hello comyn
 # / => hello world
+# /hello/(\w+)  request.args[0]
+# /hello/(?P<name>\w+)  request.kwargs.name
+# @get('/hello/(?P<name>\w+)')
